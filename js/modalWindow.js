33c5d5cfb0e4297c2cce5cc0ad12a82f2ec1f591
denstav <agressclash@gmail.com>
2022-06-03 13:57:06 +0300

    !

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,8 +1,7 @@
 const OpenBtn = document.querySelector('btn__form-cell')
 const CloseBtn = document.querySelector('close-popup')
 
-const popup-bg = document.querySelector('close-popup')
-const CloseBtn = document.querySelector('close-popup')
+
 OpenBtn.addEventListener('click',function(){
     
 })
